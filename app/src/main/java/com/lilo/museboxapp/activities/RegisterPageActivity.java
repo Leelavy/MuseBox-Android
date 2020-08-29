3b5065ad2d54443ac4257c20050b1defe7b821d1
Lilo <lilodead@gmail.com>
2020-08-29 22:40:13 +0300

    splash screen & login design done - correction

---
+++ app/src/main/java/com/lilo/museboxapp/activities/RegisterPageActivity.java
@@ -0,0 +1,16 @@
+package com.lilo.museboxapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.lilo.museboxapp.R;
+
+public class RegisterPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_page);
+    }
+}
