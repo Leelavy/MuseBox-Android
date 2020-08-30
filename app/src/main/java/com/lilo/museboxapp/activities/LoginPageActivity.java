a2406641676268f56bf977d15b11a2f53a87efde
Lilo <lilodead@gmail.com>
2020-08-30 18:24:54 +0300

    Designed post row, post details and new post pages // implemented recycler view list for feed with hardcoded posts data

--- app/src/main/java/com/lilo/museboxapp/activities/LoginPageActivity.java
+++ app/src/main/java/com/lilo/museboxapp/activities/LoginPageActivity.java
@@ -1,43 +1,46 @@
 package com.lilo.museboxapp.activities;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.lilo.museboxapp.R;
 import com.lilo.museboxapp.Utils;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class LoginPageActivity extends AppCompatActivity {
 
+    //XML views
     ImageView backgroundImageView;
     EditText emailInput;
     EditText passwordInput;
     Button loginBtn;
     Button registerBtn;
 
+    //Firebase
+    FirebaseAuth firebaseAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+
+        firebaseAuth = FirebaseAuth.getInstance();
 
         this.setTitle("Login");
 
         backgroundImageView = findViewById(R.id.login_activity_background_image_view);
         emailInput = findViewById(R.id.login_activity_email_edit_text);
         passwordInput = findViewById(R.id.login_activity_password_edit_text);
-
-        loginBtn = findViewById(R.id.login_activity_login_btn);
-        loginBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.d("TAG", "Attempt to login");
-            }
-        });
 
         registerBtn = findViewById(R.id.login_activity_register_btn);
         registerBtn.setOnClickListener(new View.OnClickListener() {
@@ -46,8 +49,44 @@ public class LoginPageActivity extends AppCompatActivity {
                 toRegisterPage();
             }
         });
+
+        loginBtn = findViewById(R.id.login_activity_login_btn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginUser();
+            }
+        });
+
         Utils.animateBackground(backgroundImageView);
 
+    }
+
+    private void loginUser(){
+        if (!emailInput.getText().toString().isEmpty() && !passwordInput.getText().toString().isEmpty()){
+            if (firebaseAuth.getCurrentUser() == null){
+                firebaseAuth.signInWithEmailAndPassword(emailInput.getText().toString(), passwordInput.getText().toString()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        Toast.makeText(LoginPageActivity.this, "Welcome!", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(LoginPageActivity.this, FragmentHomeActivity.class));
+                        LoginPageActivity.this.finish();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(LoginPageActivity.this, "Failed to login: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+            else {
+                firebaseAuth.signOut();
+                Toast.makeText(this, "Previous user signed out, please try again now", Toast.LENGTH_SHORT).show();
+            }
+        }
+        else {
+            Toast.makeText(this, "Please fill both data fields", Toast.LENGTH_SHORT).show();
+        }
     }
 
     private void toRegisterPage(){
